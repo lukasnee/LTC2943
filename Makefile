81bd35b03ae44cec4d7a6aa3eb9b3b778283fb9c
lukasnee <lukas.neverauskis@gmail.com>
2020-10-24 18:40:08 +0300

    added cmake build structure and google tests.

--- Makefile
+++
@@ -1,3 +0,0 @@
-all:
-	g++ -o LTC2943.exe main.cpp i2c.cpp LTC2943.cpp 
-	./LTC2943.exe
---
+++ tests/LTC2943Tests.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+TEST(LTC2943Tests, ChangeChipAdcModeIntoAutomaticMode) {
+    EXPECT_TRUE(true);
+}
+
+TEST(LTC2943Tests, ChangeChipAdcModeIntoScanMode) {
+    EXPECT_TRUE(true);
+}
+
+TEST(LTC2943Tests, ChangeChipAdcModeIntoManualMode) {
+    EXPECT_TRUE(true);
+}
+
+TEST(LTC2943Tests, ChangeChipAdcModeIntoSleepMode) {
+    EXPECT_TRUE(true);
+}
+
+TEST(LTC2943, GetChipAdcMode) {
+    EXPECT_TRUE(true);
+}
+
+TEST(LTC2943Tests, CheckIfTemperatureAlertIsPending) {
+    EXPECT_TRUE(true);
+}
+
+TEST(LTC2943Tests, CheckIfVoltageAlertIsPending) {
+    EXPECT_TRUE(true);
+}
